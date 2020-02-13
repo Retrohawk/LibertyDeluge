87c0857c7e86091b439f6ca5223f4d8a49a28fa7
VacatedSum <59126285+VacatedSum@users.noreply.github.com>
2020-02-13 18:21:21 -0500

    Add files via upload
    Zombie x/y coordinates not updating correctly.

--- Deluge.java
+++ Deluge.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
@@ -7,6 +10,7 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
@@ -19,22 +23,50 @@ import javafx.stage.Stage;
 public class Deluge extends Application {
 	final int MAX_X = 500;
 	final int MAX_Y = 500;
+	final int ZOMBIE_RATE = 5000;
 	final Canvas board = new Canvas(MAX_X, MAX_Y);
 	final GraphicsContext gc = board.getGraphicsContext2D();
-	ArrayList<Object>redrawArray = new ArrayList<Object>();
+	ArrayList<Zombie> zombieBag = new ArrayList<Zombie>();
 	
 	public static void main(String[] args) {
 		launch(args);
 	}
 	@Override public void start(Stage primaryStage) {
-
+		Stage myStage = new Stage();
+		myStage.setTitle("Liberty Swarm");
+		BorderPane pane = new BorderPane();
 		Player p1 = new Player(400,400);
 		p1.setColor(Color.WHITE);
-		redrawArray.add(p1);
+		Timer timer = new Timer();
+		timer.scheduleAtFixedRate(new TimerTask() {
+			@Override
+			public void run() {
+				gc.clearRect(0, 0, board.getHeight(), board.getWidth());
+				gc.setStroke((Paint) p1.getColor());
+				gc.strokeOval(p1.getX()-5, p1.getY()-5, 10, 10);
+				gc.setStroke((Paint) Color.LIGHTPINK);
+				gc.strokeLine(p1.getX(), p1.getY(), p1.getAim()[0], p1.getAim()[1]);
+				gc.setStroke((Paint) Color.GREENYELLOW);
+				for (Zombie z : zombieBag) {
+					z.setTarget(p1);
+					z.stalk();
+					System.out.println(z.getX() + " " + z.getY());
+					gc.strokeOval(z.getX()-5, z.getY()-5, 10, 10);
+				}
+			}
+		}, 0, 60);
+		
+		//Add a zombie to the board every five seconds
+		timer.scheduleAtFixedRate(new TimerTask() {
+			@Override
+			public void run() {
+				zombieBag.add(new Zombie(ZOMBIE_RATE));
+			}
+		}, 0, 5000);
 		gc.setStroke((Paint) p1.getColor());
-		//I can't seem to get the play color correct.
-		gc.setFill(Paint.valueOf(p1.getColor().toString()));
-		gc.strokeOval(p1.getX(), p1.getY(), 10, 10);
+		gc.setFill((Paint) p1.getColor());
+		gc.fillOval(p1.getX(), p1.getY(), 10, 10);
+
 		gc.strokeLine(p1.getX(), p1.getY(), p1.getAim()[0], p1.getAim()[1]);
 		gc.getCanvas().setFocusTraversable(true);
 		gc.getCanvas().addEventFilter(MouseEvent.MOUSE_MOVED, m -> {	
@@ -42,7 +74,13 @@ public class Deluge extends Application {
 			gc.clearRect(0, 0, board.getHeight(), board.getWidth());
 			gc.setStroke((Paint) p1.getColor());
 			gc.strokeOval(p1.getX()-5, p1.getY()-5, 10, 10);
+			gc.setStroke((Paint) Color.LIGHTPINK);
 			gc.strokeLine(p1.getX(), p1.getY(), p1.getAim()[0], p1.getAim()[1]);
+		});
+		gc.getCanvas().addEventFilter(MouseEvent.MOUSE_PRESSED, m -> {
+			if (m.getButton()==MouseButton.PRIMARY) {
+				//shoot?
+			}
 		});
 		gc.getCanvas().addEventFilter(KeyEvent.ANY, k -> {
 			gc.getCanvas().requestFocus();
@@ -66,23 +104,20 @@ public class Deluge extends Application {
 					p1.moveLeft();
 				}
 			}
-			if (k.getCode()==KeyCode.SPACE) {
-				//shoot that muthafucka
-			}
+			
 			gc.clearRect(0, 0, board.getHeight(), board.getWidth());
 			gc.setStroke((Paint) p1.getColor());
 			gc.strokeOval(p1.getX()-5, p1.getY()-5, 10, 10);
 			gc.strokeLine(p1.getX(), p1.getY(), p1.getAim()[0], p1.getAim()[1]);
+			
 		});
 		
-		Stage myStage = new Stage();
-		myStage.setTitle("Liberty Deluge");
-		BorderPane pane = new BorderPane();
+		
 		pane.setBackground(new Background(new BackgroundFill(Color.rgb(0, 0, 25), CornerRadii.EMPTY, Insets.EMPTY)));
 		pane.setCenter(board);
 		myStage.setScene(new Scene(pane));
 		myStage.showAndWait();
-		
+		timer.cancel();
 	}//End of start method
 	
 	
--- Zombie.java
+++ Zombie.java
@@ -3,7 +3,36 @@ public class Zombie extends Sprite {
 	private int speed;
 	private int x;
 	private int y;
+	private Player currentTarget;
 	
-	
+	public Zombie(int speed) {
+		this.x = 5;
+		this.y = 5;
+		this.speed = speed;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public void stalk() {
+		if (currentTarget.getX() < this.x) {
+			this.x -= speed;
+		}
+		else if (currentTarget.getX() > this.x) {
+			this.x += speed;
+		}
+		
+		if (currentTarget.getY() < this.y) {
+			this.y -= speed;
+		}
+		else if (currentTarget.getY() > this.y) {
+			this.y += speed;
+		}
+	}
+	public void setTarget(Player p) {
+		currentTarget = p;
+	}
 	
 }
